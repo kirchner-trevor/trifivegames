10d13760bb8fd83bd5e24a035f6cf9d8067a57c9
Trevor <kirchner.trevor@gmail.com>
2019-04-19 22:36:12 -0500

    separated out header template

--- page-header.js
+++ page-header.js
@@ -1,10 +1,7 @@
+import PageHeader from './page-header.html'
+
 export default {
-  template: `
-    <div>
-     <h1>Single-file JavaScript Component</h1>
-     <p>{{ message }}</p>
-    </div>
-  `,
+  template: PageHeader,
   data() {
     return {
       message: 'Oh hai from the component'
