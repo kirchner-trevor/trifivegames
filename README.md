52a6055a123858da81ebe2cb369e1058036088b1
Trevor <kirchner.trevor@gmail.com>
2019-09-24 21:26:09 -0500

    Renamed some stuff, added vue router.

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# sample-vue-website
+# trifivegames
--- app.js
+++ app.js
@@ -1,8 +1,18 @@
 import PageHeader from './page-header.js'
 
+const Foo = { template: '<div>foo</div>' }
+const Bar = { template: '<div>bar</div>' }
+
 new Vue({
   el: '#app',
   components: {
     PageHeader
-  }
+  },
+  router: new VueRouter({
+    routes: [
+      { path: '/', component: this },
+      { path: '/foo', component: Foo },
+      { path: '/bar', component: Bar }
+    ]
+  })
 })
