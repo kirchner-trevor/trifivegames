7a18001b86899a3f3005d9f20c4ce4d569aac0b2
Trevor <kirchner.trevor@gmail.com>
2019-12-07 20:08:38 -0600

    Added vue bootstrap.

--- page-header.js
+++ page-header.js
@@ -1,14 +1,19 @@
 export default {
   name: 'page-header',
   template: /*html*/`
-  <div>
-    <h1>Tri Five Games</h1>
-    <p>
-      <router-link to="/">Go to Home</router-link>
-      <router-link to="/foo">Go to Foo</router-link>
-      <router-link to="/bar">Go to Bar</router-link>
-      <router-link to="/game/prophetic">Go to Prophetic</router-link>
-    </p>
-  </div>
+  <b-navbar toggleable="lg" type="dark" variant="primary">
+    <b-navbar-brand href="#">Tri Five Games</b-navbar-brand>
+
+    <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+    <b-collapse id="nav-collapse" is-nav>
+      <b-navbar-nav>
+        <b-nav-item to="/">Home</b-nav-item>
+        <b-nav-item to="/foo">Foo</b-nav-item>
+        <b-nav-item to="/bar">Bar</b-nav-item>
+        <b-nav-item to="/game/prophetic">Prophetic</b-nav-item>
+      </b-navbar-nav>
+    </b-collapse>
+  </b-navbar>
   `
 }
